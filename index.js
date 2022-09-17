2a40fbe4a91b36efc619b8ed693da19a7ea03d1e
PratikshaBankar9689 <pratikshabankar5313@gmail.com>
2022-09-17 16:09:39 +0530

    completed users routes

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require ("express");
-const {users} =require ("./data/users.json");
+
+
+const usersRouter = require ("./routes/users");
+const booksRouter = require ("./routes/books");
+
+
 const app = express();
+
 const Port = 8081;
+
 app.use(express.json());
 
 app.get("/" ,(req,res) => {
@@ -10,50 +17,15 @@ app.get("/" ,(req,res) => {
     });
 });
 
-/**
- * Route:/users
- * METHOD:GET
- * Description:Get all users
- * Access:public
- * Parameter:none
- */
 
-app.get("/users",(req,res) => {
-    res.status(200).json({
-        success:true,
-        data:users,
-    });
-});
-
-
-/**
- * Route:/users/:id
- * METHOD:GET
- * Description:Get single users by id
- * Access:public
- * Parameter:id
- */
-
-app.get("/users/:id",(req,res) => {
-    const {id} =req.params
-const user = users.find((each) => each.id=== id)
-if(!user){
-    return res.status(404).json({
-        success:false,
-        message:"user not found",
-    });
-}
-return res.status(200).json({
-    success:true,
-    data:user,
-});
-});
-
+app.use("/users",usersRouter);
+app.use("/books",booksRouter);
 
 
 
 app.get("*" ,(req,res) => {
-    res.status(400).json({
+
+    res.status(404).json({
         message:"This route does not exist",
     });
 });
---
+++ routes/users.js
@@ -0,0 +1,141 @@
+const express = require("express");
+const {users} = require("../Data/users.json");
+
+const router = express.Router();
+
+/**
+ * Route:/users
+ * METHOD:GET
+ * Description:Get all users
+ * Access:public
+ * Parameter:none
+ */
+
+ router.get("/users",(req,res) => {
+    res.status(200).json({
+        success:true,
+        data:users,
+    });
+});
+
+
+/**
+ * Route:/users/:id
+ * METHOD:GET
+ * Description:Get single users by id
+ * Access:public
+ * Parameter:id
+ */
+
+router.get("/users/:id",(req,res) => {
+    const {id} =req.params
+const user = users.find((each) => each.id=== id)
+if(!user){
+    return res.status(404).json({
+        success:false,
+        message:"user not found",
+    });
+}
+return res.status(200).json({
+    success:true,
+    data:user,
+});
+});
+
+
+/**
+ * Route: /users
+ * Method: POST
+ * Description: Create new user
+ * Access: Public
+ * Parameters: none
+ */
+ router.post("/", (req, res) => {
+    const { id, name, surname, email, subscriptionType, subscriptionDate } =
+      req.body;
+  
+    const user = users.find((each) => each.id === id);
+  
+    if (user) {
+      return res.status(404).json({
+        success: false,
+        message: "User exists with this id",
+      });
+    }
+  
+    users.push({
+      id,
+      name,
+      surname,
+      email,
+      subscriptionType,
+      subscriptionDate,
+    });
+    return res.status(201).json({
+      success: true,
+      data: users,
+    });
+  });
+
+/**
+ * Route: /users/:id
+ * Method: PUT
+ * Description: Updating user data
+ * Access: Public
+ * Parameters: id
+ */
+ router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const { data } = req.body;
+  
+    const user = users.find((each) => each.id === id);
+  
+    if (!user)
+      return res.status(404).json({ success: false, message: "User not found" });
+  
+    const updatedUser = users.map((each) => {
+      if (each.id === id) {
+        return {
+          ...each,
+          ...data,
+        };
+      }
+      return each;
+    });
+  
+    return res.status(200).json({
+      success: true,
+      data: updatedUser,
+    });
+  });
+  
+
+/**
+ * Route:/users/:id
+ * METHOD:DELETE
+ * Description:Delete single users by id
+ * Access:public
+ * Parameter:id
+ */
+
+router.delete("/users/:id",(req,res) => {
+    const {id}=req.params;
+    const user=users.find((each) => each.id === id);
+ if (!user){
+    return res.status(404).json ({
+        success:false,
+        message:"User to be deleted is not found",
+    
+    });
+ }
+  const index = users.indexOf(user);
+  users.splice(index,1);
+
+  return res.status(202).json ({
+    sucess:true,
+    data:users,
+  });
+});
+
+
+ module.exports= router; 
